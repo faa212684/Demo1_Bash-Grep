e05177edb3642b6c6a043b5ecf0fb8737cfaaa38
Dustin Potter <dustinpotter60@gmail.com>
2020-11-02 21:31:25 -0600

    Added more example texts for demonstration

--- PotterDemo/demo.sh
+++ PotterDemo/demo.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 #Dustin Potter
 
-echo "Hello World"
+#Grep Example
+grep hello sample2.txt
+
+# -w Example
+grep -w is *
+grep is *
+
+# -i Example
+grep -i hello *
+grep hello *
+
+# -r Example
+grep -r This *
+
+# -v Example
+grep -v e example.txt
+
+# -l Example
+grep -l is *
+
+# -c Example
+grep -c hello *
+
+#Combination Examples
+grep -rlw is *
+grep -lvw This *
