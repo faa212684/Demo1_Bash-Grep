d27cb25cb26736f0d2b77c8c2639d2cf11d58f2f
Dustin Potter <dustinpotter60@gmail.com>
2020-11-03 13:44:46 -0600

    Comment bash code for presentation

--- PotterDemo/demo.sh
+++ PotterDemo/demo.sh
@@ -5,25 +5,25 @@
 grep hello sample2.txt
 
 # -w Example
-grep -w is *
-grep is *
+#grep -w is *
+#grep is *
 
 # -i Example
-grep -i hello *
-grep hello *
+#grep -i hello *
+#grep hello *
 
 # -r Example
-grep -r This *
+#grep -r This *
 
 # -v Example
-grep -v e example.txt
+#grep -v e example.txt
 
 # -l Example
-grep -l is *
+#grep -l is *
 
 # -c Example
-grep -c hello *
+#grep -c hello *
 
 #Combination Examples
-grep -rlw is *
-grep -lvw This *
+#grep -rlw is *
+#grep -lvw This *
