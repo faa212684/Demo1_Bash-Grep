5252c8ed8d6e6f509062a0b753b98c3c902df8e2
Dustin Potter <dustinpotter60@gmail.com>
2020-11-03 14:47:29 -0600

    update demo bash file

--- PotterDemo/demo.sh
+++ PotterDemo/demo.sh
@@ -2,15 +2,13 @@
 #Dustin Potter
 
 #Grep Example
-grep hello sample2.txt
+#grep hello sample2.txt
 
 # -w Example
 #grep -w is *
-#grep is *
 
 # -i Example
 #grep -i hello *
-#grep hello *
 
 # -r Example
 #grep -r This *
